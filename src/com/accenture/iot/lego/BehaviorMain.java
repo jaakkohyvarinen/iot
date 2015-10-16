253adcbd4296b07b88b73d5b68b64ad8b14c90d3
master <master@master-VirtualBox>
2015-10-16 20:34:32 +0300

    -

--- src/com/accenture/iot/lego/BehaviorMain.java
+++ src/com/accenture/iot/lego/BehaviorMain.java
@@ -17,23 +17,20 @@ public class BehaviorMain {
 		EV3LargeRegulatedMotor motor2 = new EV3LargeRegulatedMotor(MotorPort.C);
 		DifferentialPilot pilot = new MyPilot(2, 10, motor1, motor2, false);
 		pilot.setTravelSpeed(5);
-
 		SharedIRSensor ir = new SharedIRSensor();
 		Behavior b1 = new BehaviorForvard(pilot);
 		Behavior b2 = new BehaviorProximity(pilot, ir);
 		Behavior b3 = new BehaviorRemote(ir, pilot);
 		BehaviorBluemix b4 = new BehaviorBluemix (pilot);
 		Behavior[] behave = { b3 };
-		
 		MqttHandler handler = new MqttHandler(b4);
 		// Format: d:<orgid>:<type-id>:<device-id>
-		handler.connect("07yake.messaging.internetofthings.ibmcloud.com", "d:07yake:MQTTDevice:MQTTDevice",
-				"use-token-auth", "WOoN_njI-H76eIb&2S", true);
+		handler.connect("ubqgd7.messaging.internetofthings.ibmcloud.com", "d:ubqgd7:EV3:EV3_1",
+				"use-token-auth", "ZBb2Q0R59obhGS-dR+", true);
 		// Subscribe the Command events
 		// iot-2/cmd/<cmd-type>/fmt/<format-id>
 		handler.subscribe("iot-2/cmd/cid/fmt/json", 0);
 		pilot.addMoveListener(new TelemetryProvider(handler));
-		
 		arby = new Arbitrator(behave);
 		arby.start();
 
